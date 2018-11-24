bfda3a512b89e12bce051dc3929aa6eaae78dd5c
mmmdbybyd <915445800@qq.com>
2018-11-24 10:32:16 +0800

    Add files via upload

---
+++ SpecialProxy.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+Exit()
+{
+    echo -e "$1"
+    exit $2
+}
+
+while true
+do
+    echo -n "Please input SpecialProxy server port: "
+    read server_port
+    [ "$server_port" -gt "0" -a "$server_port" -lt "65536" ] && break
+    echo "Please input 1-65535."
+done
+apt-get -y gcc make git || yum install -y gcc make git
+git clone https://github.com/mmmdbybyd/SpecialProxy.git
+[ ! -d SpecialProxy ] && Exit "\033[41;37mdownload SpecialProxy source code failed\033[0m"
+cd SpecialProxy
+make || Exit "\033[41;37mcompile tinyproxy failed\033[0m"
+cp SpecialProxy sp
+./sp -l $server_port -p Meng && \
+Exit "\033[32mSpeciaoProxy is running.\033[0m" || \
+Exit "\033[41;37mSpeciaoProxy is stopping.\033[0m"
